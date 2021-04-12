4895ab09a649ef768246bd467081cd8d033c487f
ShadowDomo <44078427+ShadowDomo@users.noreply.github.com>
2021-04-12 16:04:38 +1000

    bring bootstrap back

--- src/Register.js
+++ src/Register.js
@@ -1,12 +1,19 @@
+import { useState } from 'react'
+
+function registerUser() {
+
+}
+
 
 const Login = () => {
+  const [username, setUsername] = useState('')
   return (
     <div className='container'>
       <form className='mt-5'>
         <h1 className='mb-4 text-center'>Register</h1>
         <div className="form-group mb-2">
-          <label >Email address</label>
-          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+          <label >Username</label>
+          <input type="text" className="form-control" aria-describedby="emailHelp" placeholder="Enter email" />
           <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
         </div>
         <div className="form-group mb-2">
@@ -23,5 +30,5 @@ const Login = () => {
   )
 }
 
-// test
+
 export default Login
