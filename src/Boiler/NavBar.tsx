1367dedac7506faeaaa10b29057519e261cca9a0
ShadowDomo <44078427+ShadowDomo@users.noreply.github.com>
2021-04-25 15:51:46 +1000

    home button routing fix

--- src/Boiler/NavBar.tsx
+++ src/Boiler/NavBar.tsx
@@ -2,9 +2,16 @@ import { Link } from "react-router-dom";
 import NavBarProfile from "./NavBarProfile";
 
 const NavBar = () => {
+  /** Returns where the home button should redirect to. */
+  function allowedUrl(): string {
+    if (localStorage.getItem("sessionID") === null) {
+      return "#";
+    }
+    return "/home";
+  }
   return (
     <div className="bg-dark navbar w-100 static-top">
-      <Link to="/home">
+      <Link to={allowedUrl}>
         <h1 className="navbar-brand mx-3 text-light">TEMPORARYNAMEgay</h1>
       </Link>
 
