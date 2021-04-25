9fabc55fcc05f8f8b005f1346c63aeb83aeff87f
ShadowDomo <44078427+ShadowDomo@users.noreply.github.com>
2021-04-25 15:50:36 +1000

    threadcard wrap text fix

--- src/Home/ThreadCard.tsx
+++ src/Home/ThreadCard.tsx
@@ -11,11 +11,14 @@ const ThreadCard = ({ thread }: Props) => {
   const content = thread.content;
   const history = useHistory();
 
+  // TODO add ... if longer than 500 chars
+  // TODO add style word wrap break word
+  // TODO limit post length
   return (
     <Link to={`/thread/${id}`}>
       <div className="border border-primary rounded my-2 p-2">
         <h4>{title}</h4>
-        <p>{content}</p>
+        <p style={{ wordWrap: "break-word" }}>{content.substring(1, 500)}</p>
       </div>
     </Link>
   );
