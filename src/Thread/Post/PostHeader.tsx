9964afcf68a650a27cb68fed45ceee177e79b81b
ShadowDomo <44078427+ShadowDomo@users.noreply.github.com>
2021-04-29 15:16:46 +1000

    fix memory leak

--- src/Thread/Post/PostHeader.tsx
+++ src/Thread/Post/PostHeader.tsx
@@ -1,6 +1,6 @@
 import { PostObj } from "../Thread";
 import prettyMS from "pretty-ms";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 interface Props {
   post: PostObj;
 }
@@ -8,10 +8,17 @@ interface Props {
 const PostHeader = ({ post }: Props) => {
   const [timeSince, setTimeSince] = useState<number>(0);
 
+  useEffect(() => {
+    let timer = setInterval(() => {
+      setTimeSince(new Date().getTime() - parseInt(post.date));
+    }, 1000);
+
+    // cleanup
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
   // continuously update how long ago the post was made
-  setInterval(() => {
-    setTimeSince(new Date().getTime() - parseInt(post.date));
-  }, 1000);
 
   return (
     <div className="row p-2 d-flex justify-content-between">
