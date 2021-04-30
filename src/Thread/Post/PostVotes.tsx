bffd5e5ae44cb9bd4fcf46aed19675d23772e423
ShadowDomo <44078427+ShadowDomo@users.noreply.github.com>
2021-04-30 14:56:40 +1000

    votes have colours

--- src/Thread/Post/PostVotes.tsx
+++ src/Thread/Post/PostVotes.tsx
@@ -10,6 +10,9 @@ interface Props {
 const PostVotes = ({ postID, username }: Props) => {
   const [votes, setVotes] = useState<number>(0);
   const server = process.env.REACT_APP_API_SERVER;
+  const [userVote, setUserVote] = useState<number>(0);
+  let upColor = "";
+  let downColor = "";
 
   useEffect(() => {
     getVotes();
@@ -25,13 +28,23 @@ const PostVotes = ({ postID, username }: Props) => {
     });
     const jsoned = await resp.json();
     setVotes(parseInt(jsoned.votes));
+
+    // set colour and upvote
+    const usersVote = await fetch(server + "getUsersVotes", {
+      body: JSON.stringify({ postID: postID, username: username }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+    });
+    const jsonedvote = await usersVote.json();
+    console.log(`set user vote to ${jsonedvote.vote}`);
+    setUserVote(jsonedvote.vote);
   }
 
   /** Upvotes or downvotes post */
   async function vote(vote: string) {
     // TODO reload post after vote
-    console.log(username);
-    console.log("vote was " + vote);
 
     const payload = {
       postID: postID,
@@ -54,17 +67,34 @@ const PostVotes = ({ postID, username }: Props) => {
   return (
     <div className="d-flex flex-column mx-2">
       <IconContext.Provider value={{ size: "2em" }}>
-        <IoIosArrowUp
-          style={{ cursor: "pointer" }}
-          onClick={(e) => vote("1")}
-        />
+        {userVote === 1 && (
+          <IoIosArrowUp
+            style={{ cursor: "pointer", color: "orange" }}
+            onClick={(e) => vote("1")}
+          />
+        )}
+        {userVote !== 1 && (
+          <IoIosArrowUp
+            style={{ cursor: "pointer" }}
+            onClick={(e) => vote("1")}
+          />
+        )}
+
         <h5 className="text-white text-center" style={{ marginBottom: "0" }}>
           {votes}
         </h5>
-        <IoIosArrowDown
-          style={{ cursor: "pointer" }}
-          onClick={(e) => vote("-1")}
-        />
+        {userVote === -1 && (
+          <IoIosArrowDown
+            style={{ cursor: "pointer", color: "blue" }}
+            onClick={(e) => vote("-1")}
+          />
+        )}
+        {userVote !== -1 && (
+          <IoIosArrowDown
+            style={{ cursor: "pointer" }}
+            onClick={(e) => vote("-1")}
+          />
+        )}
       </IconContext.Provider>
     </div>
   );
